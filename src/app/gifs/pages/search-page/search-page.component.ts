9c32c87292bdb1295458c7a5bb07fe8cbcdafe57
PECalle <PECalle@baustro.fin.ec>
2026-04-07 19:38:20 -0500

    mostrar gifs de busqueda

--- src/app/gifs/pages/search-page/search-page.component.ts
+++ src/app/gifs/pages/search-page/search-page.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { GifListComponent } from "src/app/components/gif-list/gif-list.component";
+import { GifsService } from '../../services/gifs.service';
+import { Gif } from '../../interfaces/gif.interface';
 
 @Component({
   selector: 'app-search-page.component',
-  imports: [],
+  imports: [GifListComponent],
   templateUrl: './search-page.component.html',
 })
 export default class SearchPageComponent {
+
+
+  gifService = inject(GifsService)
+  gifs = signal<Gif[]>([]);
+
+  onSearch(query: string){
+   this.gifService.searchGifs(query).
+   subscribe((resp) => {
+    console.log(resp)
+    this.gifs.set(resp);
+   })
+  }
+
+
 
 }
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -4,6 +4,7 @@ import { environment } from '@environments/environment';
 import { GiphyResponse } from '../interfaces/giphy.interfaces';
 import { Gif } from '../interfaces/gif.interface';
 import { GifMapper } from '../mapper/gif.mapper';
+import { map, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -30,10 +31,23 @@ export class GifsService {
       // console.log('hace la peticion')
       // console.log({resp})
       const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
-      console.log(gifs);
+      //console.log(gifs);
       this.trendingGifsLoading.set(false);
       this.trendingGifs.set(gifs);
     })
+  }
+
+  searchGifs(query: string){
+    return this.http.get<GiphyResponse>(`${environment.giphyUrl}/gifs/search`,{
+      params:{
+        api_key: environment.giphyApiKey,
+        limit:20,
+        q: query
+      }
+     }).pipe(
+      map(({data})=> data),
+      map((items) => GifMapper.mapGiphyItemsToGifArray(items))
+     );
   }
 
 }
